28317f472c619e61aac515b98703e523980cc604
Alex Harnett <harnettalex@gmail.com>
2023-10-09 13:10:32 -0700

    Refactoring calculator.rb

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -1,24 +1,89 @@
-#Get two numbers from the user
-puts "Give a number"
-number1 = gets.chomp.to_i
-puts "Give me a second number"
-number2 = gets.chomp.to_i
-
-#Asks for the type of operation to perform: add, subtract, multiply or divide
-puts "What operation would you like to perform(add, subtract, multiply or divide):"
-operation = gets.chomp
-
-#Display the result
-case operation
-when "add" then puts (number1 + number2).to_s
-when "subtract" then puts (number1 - number2).to_s
-when "mulitply" then puts (number1 * number2).to_s
-when "divide" 
-  if(number2 != 0)
-    puts (number1.to_f / number2.to_f).to_s
-  else
-    puts "When using the divide operation the second number may not be a zero!"
-  end
-else
-  puts "You did not enter a valid operation"
+def prompt(message)
+  puts "=> #{message}"
 end
+
+def valid_number?(num)
+  num.to_i != 0
+end
+
+def operation_to_message(op)
+  case op
+  when 'add'
+    "Adding"
+  when 'subtract'
+    "Subtracting"
+  when 'multiply'
+    "Multiplying"
+  when 'divide'
+    "Dividing"
+  end
+end
+
+prompt("Welcome to calculator! Enter your name: ")
+name = ''
+loop do
+  name = gets.chomp
+  if name.empty?()
+    prompt("Make sure you enter a valid name.")
+  else
+    break
+  end
+end
+
+prompt("Hi #{name}")
+
+loop do # main loop
+  # Get two numbers from the user
+  number1 = ''
+  loop do
+    prompt("Give a number")
+    number1 = gets.chomp.to_i
+    if valid_number?(number1)
+      break
+    else
+      prompt("You didn't input a valid number")
+    end
+  end
+
+  number2 = ''
+  loop do
+    prompt("Give me a second number")
+    number2 = gets.chomp.to_i
+    if valid_number?(number2)
+      break
+    else
+      prompt("You didn't input a valid number")
+    end
+  end
+
+  # Asks for the type of operation to perform: add, subtract, multiply or divide
+  prompt("What operation would you like to perform
+    (add, subtract, multiply or divide):")
+  operation = ''
+  loop do
+    operation = gets.chomp
+    if ['add', 'subtract', 'multiply', 'divide'].include?(operation)
+      break
+    else
+      prompt("You did not type a valid operation")
+    end
+  end
+
+  prompt("#{operation_to_message(operation)} the two numbers...")
+
+  # Display the result
+  result = case operation
+           when "add" then number1 + number2
+           when "subtract" then number1 - number2
+           when "mulitply" then number1 * number2
+           when "divide" then number1.to_f / number2.to_f
+           end
+
+  prompt("The result is #{result}")
+
+  prompt("Do you want to perform another calculation?(Y to go again)")
+  answer = gets.chomp
+  break unless answer.downcase().start_with?('y')
+end
+
+prompt("Goodbye")
