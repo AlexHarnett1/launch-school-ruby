f61b8bcffe3d1c617d53292226352b3572547c0c
Alex Harnett <harnettalex@gmail.com>
2024-07-11 19:54:50 +0800

    Index, new, delete funcitonality for contact_list

--- RB175/lesson_8/contact_list/contact_list.rb
+++ RB175/lesson_8/contact_list/contact_list.rb
@@ -3,11 +3,39 @@ require "sinatra/reloader"
 require "sinatra/content_for"
 require "tilt/erubis"
 
+root = File.expand_path("..", __FILE__)
+
 configure do
   enable :sessions
   set :session_secret, 'secret123412341234123412341234123412341234123412341234123412341234123412341234123412341234123412341234'
 end
 
-get "/" do 
-  "Hello world"
+before do
+  session[:contacts] ||= []
+end
+
+get "/" do
+  erb :index
+end
+
+get "/new" do 
+  erb :new_contact
+end
+
+
+# Need to add validations here for the fields...
+# Need to create an id for each contact for deletion/reordering
+post "/new" do 
+  contact_info = {name: params[:name], number: params[:number], email: params[:email]}
+  session[:contacts] << contact_info
+  session[:message] = "A new contact was added."
+  redirect "/"
+end
+
+
+
+post "/delete" do 
+  session[:contacts].delete_at(params[:id].to_i)
+  session[:message] = "Contact deleted."
+  redirect "/"
 end
